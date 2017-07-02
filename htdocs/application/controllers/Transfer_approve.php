6d7daddf1b5bfa89190ed698d9810a7c7da1c9ae
krittkarin <krittkarin@gmail.com>
2017-07-02 10:05:54 +0700

    View transfer & approve

--- htdocs/application/controllers/Transfer_approve.php
+++ htdocs/application/controllers/Transfer_approve.php
@@ -40,6 +40,38 @@ class Transfer_approve extends Auth_Controller {
         $this->data['blade'] = "transfer/transfer_approval_display";
         $this->_render_page('template/content', $this->data);
     }
+    
+    public function view($order_no) {
+        $this->data['title'] = 'View transfer : ' . $order_no;
+
+        $this->data['order'] = $this->order_model->get_order($order_no)->result();
+        $order_item = $this->order_model->get_order_item($order_no)->result();
+
+        if (count($order_item) <= 0)
+            redirect('order', 'refresh');
+
+        //Set  Table template
+        $template = array(
+            'table_open' => '<table class="table table-hover" cellspacing="0" width="100%">'
+        );
+        $this->table->set_template($template);
+        $this->table->set_heading('NO.', 'ITEM', 'DESCRIPTION', 'UNIT PRICE', 'QTY', 'TOTAL');
+        //list Category
+        foreach ($order_item as $k => $v) {
+            $this->table->add_row($k + 1, $v->product_name, $v->product_desc, number_format($v->unit_price, 2), number_format($v->quantity, 2), number_format($v->amount, 2));
+        }
+
+        $this->table->add_row('SUB TOTAL', '', '', '', '', '<strong>' . number_format($order_item[0]->order_subtotal, 2) . '</strong>');
+        $this->table->add_row('DISCOUNT', '', '', '', '', number_format($order_item[0]->order_discount, 2));
+        $this->table->add_row('TAX', '', '', '', '', number_format($order_item[0]->order_tax, 2) . '%');
+        //$this->table->add_row('<>TOTAL</strong>','','','',  '<strong>'.number_format($order_item[0]->order_total,2).'</strong>' );
+        $this->data['order_item_list'] = $this->table->generate();
+
+
+        $this->data['order_no'] = $order_no;
+        $this->data['blade'] = "transfer/transfer_approval_view_detail";
+        $this->_render_page('template/content', $this->data);
+    }
 
     public function approve($order_no, $action) {
         //Save action
